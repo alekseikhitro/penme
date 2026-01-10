d92978d0c7f3fedd56976043c9bb3deb0f5920dc
Aliaksei Khitro <leshahitro@gmail.com>
2026-01-10 22:14:34 +0000

    Initial Commit

---
+++ penme/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  penme
+//
+//  Created by Aliaksei on 10/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ penme/penmeApp.swift
@@ -0,0 +1,32 @@
+//
+//  penmeApp.swift
+//  penme
+//
+//  Created by Aliaksei on 10/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct penmeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
